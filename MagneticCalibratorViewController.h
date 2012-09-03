48cfd1740c1cbadc423e755f5f8d3d85c87e0221
Marcio Mendes <mmp.mendes@campus.fct.unl.pt>
2012-09-03 15:58:16 +0100

    YAY NICE COMMIT

--- MagneticCalibratorViewController.h
+++ MagneticCalibratorViewController.h
@@ -4,6 +4,8 @@
 //
 //  Created by GNSSLab on 13/08/12.
 //  Copyright (c) 2012 GNSSLab. All rights reserved.
+
+//  These are my changes to test git
 //
 
 #import "MagneticCalibratorAppDelegate.h"
